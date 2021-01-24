463c49d29addf44a18ae256fb98926fabe8ff069
Arun Prakash G <66074345+arunprakash09@users.noreply.github.com>
2021-01-24 14:24:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var secDiv = document.getElementById('sec');
+var minDiv = document.getElementById('min');
+var hourDiv = document.getElementById('hour');
+setInterval(clock, 1000);
+function clock(){
+	let date = new Date();
+	let sec = date.getSeconds() / 60;
+	let min = (date.getMinutes() + sec) / 60;
+	let hour = (date.getHours() + min) / 12;
+
+	secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
+	minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
+	hourDiv.style.transform = "rotate(" + (hour * 360) + "deg)";
+}
+clock();
